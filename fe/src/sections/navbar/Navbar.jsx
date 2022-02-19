f1226996df1db73e07e18516ab4ad8688627821c
Alfred Jophy <alfredjophy@protonmail.com>
2022-02-19 20:56:38 +0530

    cleanup

--- fe/src/sections/navbar/Navbar.jsx
+++ fe/src/sections/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import style from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const views = ["Dashboard", "Results", "UploadFile"];
     return (
         <div>
             <h3>Dashboard</h3>
