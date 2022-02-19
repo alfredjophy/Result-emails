834b9a451c83d1b8386c0ad8512ef4eea0170a6b
Alfred Jophy <alfredjophy@protonmail.com>
2022-02-19 16:45:03 +0530

    init react app

---
+++ fe/result_emails/src/sections/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import style from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>Dashboard</h3>
+            <h3>Upload New File</h3>
+        </div>
+    );
+};
