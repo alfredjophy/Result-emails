8ddab2d18e40ad661f467a988d8873c5f0795745
Alfred Jophy <alfredjophy@protonmail.com>
2022-02-19 18:03:31 +0530

    finished backend

--- be/db_functions.py
+++ be/db_functions.py
@@ -11,17 +11,16 @@ db = pymysql.connect(
 )
 cur = db.cursor()
 
-def add_result_to_db(name) : 
+def add_result_to_db(name,records) : 
+    # fix CURDATE()
     cur.execute('insert into results(name,uploadDate,emailSent) values(\'{0}\',CURDATE(),false)'.format(name))
-    db.commit()
-    return True
-
-def add_records_to_db(fileName,records) :
+    # this works 
     subjects=list(filter(lambda x: x not in ['SI No','Name','Email'],(records[0].keys())))
-    cur.execute('update results set subjects = \'{0}\' where Name = \'{1}\''.format(reduce(lambda str,subject :'{0},{1}'.format(str,subject.strip()),subjects),fileName))
-    subjectsMachineR=list(map(lambda x : x.replace(' ',''),subjects))
+    cur.execute('update results set subjects = \'{0}\' where Name = \'{1}\''.format(reduce(lambda str,subject :'{0},{1}'.format(str,subject.strip()),subjects),name))
+    
+    subjectsMachineR=list(map(lambda x : x.strip().replace(' ','_'),subjects))
     subject_args=reduce(lambda str, subject :'{0},{1} varchar(50)'.format(str,subject),subjectsMachineR,'')
-    cur.execute('create table {0}(SI_No varchar(20),Name varchar(35),Email varchar(45){1},emailRead boolean default false)'.format(fileName,subject_args))
+    cur.execute('create table {0}(SI_No varchar(20),Name varchar(35),Email varchar(45){1},emailRead boolean default false)'.format(name,subject_args))
     
     for i in records : 
         values = '\'{0}\',\'{1}\',\'{2}\''.format(i['SI No'],i['Name'],i['Email'])
@@ -32,9 +31,28 @@ def add_records_to_db(fileName,records) :
                 values+=',NULL'
             else:
                 values+=','+mark
-        cur.execute('insert into {0} values({1},false)'.format(fileName,values))
+        cur.execute('insert into {0} values({1},false)'.format(name,values))
     db.commit()
     return True
 
-    
+def get_result_from_db(name):
+    # fix the LIKE 
+    try: 
+        cur.execute('select * from results where name like \'{0}\''.format(name))
+        resultInfo = cur.fetchall()  
+    except:
+        return [False]
+    if(resultInfo == ()):
+        return [False]
+
+    subjects=(resultInfo[0]['subjects']).split(',')
+    cur.execute('select * from {0}'.format(name))
+    records = cur.fetchall()
+    return [True,{'resultInfo' : {'emailSent' : resultInfo[0]['emailSent'],
+        'uploadDate' : resultInfo[0]['uploadDate'],'subjects' : subjects },'records':records}]
+
+def get_results_from_db():
+    cur.execute('select name,uploadDate,emailSent from results order by uploadDate')
+    results = cur.fetchall()
+    return results
 
--- be/server.py
+++ be/server.py
@@ -2,7 +2,9 @@
 from flask import Flask,jsonify,request
 import os
 import pathlib
-from db_functions import add_result_to_db,add_records_to_db
+
+from db_functions import add_result_to_db,get_result_from_db,get_results_from_db
+
 from utils.excel import verifySheet,getRecords
 from utils.generateFileName import generateFileName
 
@@ -29,10 +31,21 @@ def upload_sheet() :
     fileName = generateFileName(metadata)
     file.save(FILE_STORAGE_PATH/'{0}'.format(fileName))
 
-    add_result_to_db(fileName)
-    add_records_to_db(fileName,getRecords(file))
+    add_result_to_db(fileName,getRecords(file))
+    return jsonify({'resultName' : fileName}),200
 
-    return jsonify({'fileName' : fileName}),200
+@app.route('/get_result/<resultName>',methods=['GET'])
+def get_result(resultName) : 
+    result = get_result_from_db(resultName)
+    if not result[0] :
+        return jsonify({"error" : "result not found"}),404
+    return jsonify(result[1]),200
+
+@app.route('/get_results',methods=['GET'])
+def get_results() : 
+    results = get_results_from_db()
+    return jsonify(results),200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
