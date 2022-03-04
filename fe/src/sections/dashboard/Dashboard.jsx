026c3b0f8ee98e3121ce4a21daa46a3a41588575
Alfred Jophy <alfredjophy@protonmail.com>
2022-03-05 04:42:45 +0530

    fixed dep read stats

--- fe/src/sections/dashboard/Dashboard.jsx
+++ fe/src/sections/dashboard/Dashboard.jsx
@@ -5,11 +5,18 @@ import { useDepartmentsQuery, useDepartmentStatsQuery } from "../../queries";
 const DepartmentCard = (props) => {
     const stats = useDepartmentStatsQuery(props.d.name);
     if (stats.isLoading) return <h4>Loading...</h4>;
+    console.log(stats.data.readStats);
     return (
         <Link className={style.link} to={`/departments/${props.d.name}`}>
             <div className={style.card}>
                 <h4 className={style.sub}>{props.d.name}</h4>
-                <h4 className={style.percent}>{stats.data.readStats} %</h4>
+
+                {(stats.data.readStats && (
+                    <h4 className={style.percent}>
+                        {stats.data.readStats.toFixed(2)} %
+                    </h4>
+                )) ||
+                    null}
             </div>
         </Link>
     );
